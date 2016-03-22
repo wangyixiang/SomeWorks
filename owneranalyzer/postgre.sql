ac7f0372494bb2a9f7a63501209b984832312976
wangyixiang <wangyixiang.linux@qq.com>
2016-03-22 17:29:45 +0800

    I should use '' to wrap the default value and check the related existence.

--- owneranalyzer/postgre.sql
+++ owneranalyzer/postgre.sql
@@ -1,20 +1,20 @@
-CREATE TABLE files (
+CREATE TABLE IF NOT EXISTS files (
     id serial PRIMARY KEY,
     absolute_name varchar NOT NULL,
-    unc_name varchar NOT NULL DEFAULT "",
+    unc_name varchar NOT NULL DEFAULT '',
     basename varchar NOT NULL,
-    dirname varchar NOT NULL,
-    ownername varchar NOT NULL DEFAULT "",
-    domainname varchar NOT NULL DEFAULT "",
+    dir_name varchar NOT NULL,
+    owner_name varchar NOT NULL DEFAULT '',
+    domain_name varchar NOT NULL DEFAULT '',
     last_modified_time timestamp NOT NULL,
-    filesize bigint NOT NULL,
+    file_size bigint NOT NULL,
     is_file boolean NOT NULL DEFAULT TRUE,
     is_exists boolean NOT NULL DEFAULT TRUE,
-    os varchar NOT NULL DEFAULT "windows",
+    os varchar NOT NULL DEFAULT 'windows',
     UNIQUE(absolute_name)
 );
-CREATE INDEX files_absolute_name_index ON files(absolute_name);
-CREATE INDEX files_basename_index ON files(basename);
-CREATE INDEX files_dir_name_index ON files(dirname);
-CREATE INDEX files_ownername_index ON files(ownername);
-CREATE INDEX files_domainname_index on files(domainname);
+CREATE INDEX IF NOT EXISTS files_absolute_name_index ON files(absolute_name);
+CREATE INDEX IF NOT EXISTS files_basename_index ON files(basename);
+CREATE INDEX IF NOT EXISTS files_dir_name_index ON files(dir_name);
+CREATE INDEX IF NOT EXISTS files_owner_name_index ON files(owner_name);
+CREATE INDEX IF NOT EXISTS files_domain_name_index on files(domain_name);
